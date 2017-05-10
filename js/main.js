e9830ce4b91c61236ce8ef3e287f17624b309bbf
Linda Call <lindacall4home@hotmail.com>
2017-05-10 14:08:14 -0600

    finish test scoring

--- js/main.js
+++ js/main.js
@@ -1,23 +1,5 @@
 $(document).ready(function(){
-
-  function  ArtistTest(numberQuestions){
-    this.numberQuestions = numberQuestions;
-    this.currentQuestion = 1;
-    this.artistIndex = 0;
-    this.artistQuestions = [];
-    this.getNextArtist = function(){
-      return artists[this.artistIndex++];
-    };
-    this.getArtistWork = function(artist){
-      return artist.works[0];
-    };
-    for(i = 0; i < numberQuestions; i++){
-      let nextArtist = this.getNextArtist();
-      let nextQuestion = new ArtistQuestion(nextArtist, this.getArtistWork(nextArtist));
-      this.artistQuestions.push(nextQuestion);
-    }
-  }
-
+  var artistTest;
 
   function renderArtists(){
     for(let i = 0; i < artists.length; i++){
@@ -97,16 +79,115 @@ $(document).ready(function(){
     this.artist = artist;
     this.artistWork = artistWork;
     this.studentAnswer = "";
+    this.answerCorrect = false;
+  }
+
+  function  ArtistTest(numberQuestions){
+    this.numberQuestions = numberQuestions;
+    this.currentQuestion = 0;
+    this.artistIndex = 0;
+    this.artistQuestions = [];
+    this.getNextArtist = function(){
+      return artists[this.artistIndex++];
+    };
+    this.getArtistWork = function(artist){
+      return artist.works[0];
+    };
+    for(i = 0; i < numberQuestions; i++){
+      let nextArtist = this.getNextArtist();
+      let nextQuestion = new ArtistQuestion(nextArtist, this.getArtistWork(nextArtist));
+      this.artistQuestions.push(nextQuestion);
+    }
   }
 
   function gradeAnswer(){
-    console.log("grading: " + artistTest.artistQuestions[artistTest.currentQuestion - 1].artist.fullName);
-    
     var artistQuestion = artistTest.artistQuestions[artistTest.currentQuestion - 1];
+
     $("#answer-modal-label").text("Question " + artistTest.currentQuestion + " of " + artistTest.numberQuestions);
+
+    if(artistQuestion.studentAnswer === artistQuestion.artist.fullName){
+      artistQuestion.answerCorrect = true;
+      showAnswerIsCorrect();
+    }
+    else{
+      artistQuestion.answerCorrect = false;
+      showAnswerIsIncorrect();
+    }
+
+    $('#correct-answer-image').attr("src", artistQuestion.artist.thumbnail);
+    $('#correct-answer-title').text(artistQuestion.artist.fullName + ":  " + artistQuestion.artistWork.workTitle);
+
+  }
+
+  function showAnswerIsCorrect(){
+    $('#answer-correct').removeClass('hide-grade');
+    $('#answer-correct').addClass('show-grade');
+    $('#answer-wrong').removeClass('show-grade');
+    $('#answer-wrong').addClass('hide-grade');
+  }
+
+  function showAnswerIsIncorrect(){
+    $('#answer-correct').removeClass('show-grade');
+    $('#answer-correct').addClass('hide-grade');
+    $('#answer-wrong').removeClass('hide-grade');
+    $('#answer-wrong').addClass('show-grade');
+  }
+
+  function displayScore(){
+    let numberCorrect = 0;
+    let scoreText = '';
+    let scoreComment = '';
+    let scoreGrade = '';
+
+    for(let i = 0; i < artistTest.artistQuestions.length; i++){
+      if(artistTest.artistQuestions[i].answerCorrect){
+        numberCorrect++;
+      }
+    }
+
+    scoreText = 'You correctly identified '+ numberCorrect + ' out of ' + artistTest.artistQuestions.length + ' artists';
+
+    let percentCorrect = numberCorrect/artistTest.artistQuestions.length * 100;
+
+    if(percentCorrect >= 90){
+      scoreText = scoreText + '!';
+      scoreGrade = 'Awesome!';
+      scoreComment = 'You are a master at identifying composers!';
+    }
+    else if(percentCorrect >= 80){
+      scoreText = scoreText + '!';
+      scoreGrade = 'Great job!';
+      scoreComment = 'You have learned a lot about classical composers!';
+    }
+    else if(percentCorrect >= 70){
+      scoreText = scoreText + '.';
+      scoreGrade = 'Nice work.';
+      scoreComment = 'Keep studying to improve your score!';
+    }
+    else {
+      scoreText = scoreText + '.';
+      scoreGrade = 'What Happened!';
+      scoreComment = 'You should study more!';
+    }
+    $('#final-score').text(scoreText);
+    $('#score-grade').text(scoreGrade);
+    $('#score-comment').text(scoreComment);
   }
 
   function displayQuestion(){
+    console.log(artistTest);
+    artistTest.currentQuestion++;
+    // if(artistTest.currentQuestion > artistTest.artistQuestions.length){
+    //   displayScore();
+    //   return;
+    // }
+    // else if(artistTest.currentQuestion === artistTest.artistQuestions.length){
+    //   $('#next-question').text('Get Score');
+    // }
+    // else{
+    //   $('#next-question').text('Next Question');
+    // }
+
     console.log("question: " + artistTest.artistQuestions[artistTest.currentQuestion - 1].artist.fullName);
 
     var artistQuestion = artistTest.artistQuestions[artistTest.currentQuestion - 1];
@@ -127,6 +208,12 @@ $(document).ready(function(){
         $('#answer-image' + wrong).attr("src", artists[wrong].thumbnail);
       }
     }
+    $('.select-artist').prop("checked", false);
+  }
+
+  function administerTest(){
+    artistTest = new ArtistTest(3);
+    displayQuestion();
   }
 
   function addEventListeners(){
@@ -138,15 +225,16 @@ $(document).ready(function(){
 
     $('#more-artist-info').click(displayMoreArtistInfo);
     $('#close-more-info').click(closeMoreArtistInfo);
-    $('#start-test').click(displayQuestion);
+    $('#start-test').click(administerTest);
+    $('#next-question').click(displayQuestion);
+    $('#display-score').click(displayScore);
     $('#submit-answer').click(gradeAnswer);
-    $('.test-answer').click(function(event){
+    $('.select-artist').click(function(event){
       artistTest.artistQuestions[artistTest.currentQuestion - 1].studentAnswer = $(event.currentTarget).next().next().text();
       console.log(artistTest);
     });
   }
 
-  var artistTest = new ArtistTest(10);
   addEventListeners();
   renderArtists();
   populateSelectedArtist(artists[0]);
